8f4b425c0ce506895cc06d18d07e7d286e6bc5d7
유민호 <wingedcompany@gmail.com>
2022-04-29 01:04:32 +0900

    VITE 해보기 + QUASAR 2

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,21 +1,97 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import {ref} from 'vue'
+
+const tab = ref('images');
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div>
+    <q-layout view="lHr LpR lFr" class="shadow-2 rounded-borders">
+
+      <q-header bordered class="bg-primary text-white max-width">
+        <q-toolbar>
+          <q-toolbar-title class="text-center">
+            기록를 더 즐겁게, 코드크루
+          </q-toolbar-title>
+        </q-toolbar>
+      </q-header>
+
+      <q-page-container class="max-width">
+        <q-page class="q-pa-md">
+          <p v-for="n in 15" :key="n">
+            <q-card class="my-card">
+              <q-card-section>
+                <q-item>
+                  <q-item-section side>
+                    <q-avatar rounded color="primary" text-color="white">
+                      그
+                      <q-badge floating color="orange">N</q-badge>
+                    </q-avatar>
+                  </q-item-section>
+                  <q-item-section>
+                    <q-item-label>
+                      <q-item style="padding:0; min-height: 0">
+                        <q-item-section>그룹 이름</q-item-section>
+                        <q-item-section side>1시간 전</q-item-section>
+                      </q-item>
+                    </q-item-label>
+                    <q-item-label caption>그룹에 대한 설명을 적는 공간입니다.</q-item-label>
+                  </q-item-section>
+                </q-item>
+              </q-card-section>
+
+              <q-card-section>
+                <div class="row" style="text-align: center">
+                  <div class="col">
+                    티스토리 (2)
+                  </div>
+                  <div class="col">
+                    벨로그 (1)
+                  </div>
+                  <div class="col">
+                    브런치 (3)
+                  </div>
+                </div>
+              </q-card-section>
+
+              <q-separator/>
+
+              <q-card-actions vertical>
+                <q-btn flat>Action 1</q-btn>
+                <q-btn flat>Action 2</q-btn>
+              </q-card-actions>
+            </q-card>
+          </p>
+        </q-page>
+      </q-page-container>
+
+      <q-footer bordered class="bg-white text-primary max-width">
+        <q-tabs no-caps active-color="primary" indicator-color="transparent" class="text-grey" v-model="tab">
+          <q-tab name="images" label="라운지"/>
+          <q-tab name="videos" label="그룹"/>
+          <q-tab name="articles" label="설정"/>
+        </q-tabs>
+      </q-footer>
+
+    </q-layout>
+  </div>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  font-family: "Noto Sans KR", Avenir, Helvetica, Arial, sans-serif;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+ul {
+  padding: 5px;
+  list-style-type: none;
+}
+
+.max-width {
+  width: 460px;
+  margin: 0 auto;
+  border-right: 1px solid #eee;
+  border-left: 1px solid #eee;
 }
 </style>
