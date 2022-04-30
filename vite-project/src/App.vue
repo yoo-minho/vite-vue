72060f5728322f1f2ba5a9b1b81bf046dedbb116
유민호 <wingedcompany@gmail.com>
2022-04-30 22:31:40 +0900

    VITE 해보기 + QUASAR 2

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,72 +1,22 @@
-<script setup>
-import {ref} from 'vue'
-
-const tab = ref('images');
-</script>
-
 <template>
   <div>
     <q-layout view="lHr LpR lFr" class="shadow-2 rounded-borders">
 
       <q-header bordered class="bg-primary text-white max-width">
         <q-toolbar>
-          <q-toolbar-title class="text-center">
-            기록를 더 즐겁게, 코드크루
+          <q-toolbar-title class="">
+            Codecrew.io
           </q-toolbar-title>
+          <q-btn flat round dense icon="search" />
+          <q-btn flat round dense icon="add_circle_outline" />
         </q-toolbar>
       </q-header>
 
       <q-page-container class="max-width">
         <q-page class="q-pa-md">
           <p v-for="n in 15" :key="n">
-            <q-card class="my-card">
-              <q-card-section>
-                <q-item>
-                  <q-item-section side>
-                    <q-avatar rounded color="primary" text-color="white">
-                      그
-                      <q-badge floating color="orange">N</q-badge>
-                    </q-avatar>
-                  </q-item-section>
-                  <q-item-section>
-                    <q-item-label>
-                      <q-item style="padding:0; min-height: 0">
-                        <q-item-section>그룹 이름2</q-item-section>
-                        <q-item-section side>1시간 전</q-item-section>
-                      </q-item>
-                    </q-item-label>
-                    <q-item-label caption>그룹에 대한 설명을 적는 공간입니다.</q-item-label>
-                  </q-item-section>
-                </q-item>
-              </q-card-section>
-
-              <q-card-section>
-                <div class="row" style="text-align: center">
-                  <div class="col">
-                    티스토리 (2)
-                  </div>
-                  <div class="col">
-                    벨로그 (1)
-                  </div>
-                  <div class="col">
-                    브런치 (3)
-                  </div>
-                </div>
-              </q-card-section>
-
-              <q-separator/>
-
-              <q-card-actions vertical>
-                <q-btn flat>Action 1</q-btn>
-                <q-btn flat>Action 2</q-btn>
-              </q-card-actions>
-            </q-card>
+            <GroupCard/>
           </p>
-
-          <q-page-sticky position="bottom-right" :offset="[18, 18]">
-            <q-btn fab icon="add" color="accent" />
-          </q-page-sticky>
-
         </q-page>
       </q-page-container>
 
@@ -81,6 +31,10 @@ const tab = ref('images');
     </q-layout>
   </div>
 </template>
+
+<script setup>
+import GroupCard from './components/GroupCard.vue'
+</script>
 
 <style>
 #app {
---
+++ vite-project/src/components/GroupCard.vue
@@ -0,0 +1,52 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <q-card class="my-card">
+    <q-card-section>
+      <q-item>
+        <q-item-section side>
+          <q-avatar rounded color="primary" text-color="white">
+            그
+            <q-badge floating color="orange">N</q-badge>
+          </q-avatar>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>
+            <q-item style="padding:0; min-height: 0">
+              <q-item-section>그룹 이름2</q-item-section>
+              <q-item-section side>1시간 전</q-item-section>
+            </q-item>
+          </q-item-label>
+          <q-item-label caption>그룹에 대한 설명을 적는 공간입니다.</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-card-section>
+
+    <q-card-section>
+      <div class="row" style="text-align: center">
+        <div class="col">
+          티스토리 (2)
+        </div>
+        <div class="col">
+          벨로그 (1)
+        </div>
+        <div class="col">
+          브런치 (3)
+        </div>
+      </div>
+    </q-card-section>
+
+    <q-separator/>
+
+    <q-card-actions vertical>
+      <q-btn flat>Action 1</q-btn>
+      <q-btn flat>Action 2</q-btn>
+    </q-card-actions>
+  </q-card>
+</template>
+
+<style>
+</style>
