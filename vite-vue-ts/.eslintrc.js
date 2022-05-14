25e344f2f7b17aeb83618e2ccd6580b0f07334a4
유민호 <wingedcompany@gmail.com>
2022-05-15 01:01:34 +0900

    issue complete

--- vite-vue-ts/.eslintrc.js
+++ vite-vue-ts/.eslintrc.js
@@ -1,11 +1,26 @@
 module.exports = {
-  //parser: 'vue-eslint-parser',
-  //parserOptions: { parser: '@typescript-eslint/parser' },
-  extends: ['plugin:vue/vue3-recommended', '@vue/typescript/recommended', 'prettier'],
-  plugins: ['@typescript-eslint'],
+  root: true,
   env: {
     node: true,
   },
-  root: true,
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'plugin:vue/vue3-recommended',
+    '@vue/typescript/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    parser: require.resolve('@typescript-eslint/parser'),
+    extraFileExtensions: ['.vue'],
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'no-unused-vars': 'off',
+      },
+    },
+  ],
   rules: {},
 };
